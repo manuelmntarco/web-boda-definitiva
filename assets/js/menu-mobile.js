912973fb7e3a61e37ba445a3d8a0b66cf29b4cf5
Manuel <manuelmntarco@gmail.com>
2025-07-04 19:54:44 +0200

    Web con JS terminado. Version final. Sexta subida

---
+++ assets/js/menu-mobile.js
@@ -0,0 +1,35 @@
+
+document.addEventListener("DOMContentLoaded", (event) =>{
+
+    // Seleccionamos mis dos elementos principales:
+    let mobile_btn = document.querySelector(".navbar__mobile-btn");
+    let mobile_menu = document.querySelector(".menu-mobile");
+
+    // Función mostrar y ocultar menú:
+    const showHiddenMenu = () => {
+        let show = document.querySelector(".menu-mobile--show");
+
+        if (show){
+            mobile_menu.classList.remove("menu-mobile--show");
+        }else{
+            mobile_menu.classList.add("menu-mobile--show");
+        }
+    };
+
+    // Al dar click al botón del menú, mostrar el menu de navegación responsive
+    mobile_btn.addEventListener("click", showHiddenMenu);
+
+    // Al redimensionar la pantalla ocultar el menu si es necesario:
+    window.addEventListener("resize", () => {
+        let window_width = parseInt(document.body.clientWidth);
+
+        if(window_width >= 1000){
+            mobile_menu.classList.remove("menu-mobile--show");
+        }
+    });
+
+    // Poder cerrar el menú con el botón X:
+    let btn_close = document.querySelector(".menu-mobile__close");
+
+    btn_close.addEventListener("click", showHiddenMenu);
+});
